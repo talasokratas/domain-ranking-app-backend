98fb2f9ed93cc7e83e18f91dc01da9709a9c34d8
Evaldas Vaitonis <evaldas@rinkodara.lt>
2024-09-14 18:32:19 +0300

    PullPageRanks job created with handle logic

---
+++ app/Jobs/PullPageRanksJob.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Exceptions\DomainsListFileNotFoundException;
+use App\Exceptions\OpenPageRankServiceException;
+use App\Models\DomainInfo;
+use App\Services\OpenPageRankService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+
+class PullPageRanksJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * @throws DomainsListFileNotFoundException
+     * @throws OpenPageRankServiceException
+     */
+    public function handle(): void
+    {
+        $response = Http::get(env('DOMAIN_LIST_JSON_FILE', false));
+        if ($response->successful()) {
+            $websites_list = $response->json();
+        } else {
+            throw new DomainsListFileNotFoundException();
+        }
+
+        $domains = collect($websites_list)->pluck('rootDomain')->toArray();
+        $rankService = new OpenPageRankService();
+
+        $rank_data = $rankService->getPagesData($domains);
+
+        foreach ($rank_data as $page ) {
+            $domainInfo = DomainInfo::where('name', $page->domain)->first();
+            if(!$domainInfo) {
+                $domainInfo = new DomainInfo();
+            }
+            $domainInfo->name = $page->domain;
+            $domainInfo->rank = $page->rank;
+            $domainInfo->save();
+        }
+    }
+}
